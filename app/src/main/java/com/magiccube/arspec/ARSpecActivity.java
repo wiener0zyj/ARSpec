dfbd82b473f62cdcc07fe27e69b54dc65a6a29c8
Yunjian Zhang <445892688@qq.com>
2019-04-20 15:32:30 +0800

    init the android repository

---
+++ app/src/main/java/com/magiccube/arspec/ARSpecActivity.java
@@ -0,0 +1,17 @@
+package com.magiccube.arspec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ARSpecActivity extends AppCompatActivity {
+
+    private Button btn_camera;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arspec);
+
+        btn_camera = (Button) findViewById(R.id.open_camera_button);
+    }
+}
